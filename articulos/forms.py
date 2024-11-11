c1e4b3aaa27c2e0dbc1a2181ac354571b56065c5
SebastianEspinozaSolis <Sebastian.e.s.programador@gmail.com>
2024-11-10 22:30:12 -0300

    costo añadido

--- articulos/forms.py
+++ articulos/forms.py
@@ -4,9 +4,10 @@ from .models import Publicacion
 class PublicacionForm(forms.ModelForm):
     class Meta:
         model = Publicacion
-        fields = ['titulo', 'descripcion', 'imagen']
+        fields = ['titulo', 'descripcion','costo', 'imagen']
         widgets = {
             'titulo': forms.TextInput(attrs={'class': 'form-control'}),
             'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
+            'costo': forms.NumberInput(attrs={'class': 'form-control'}),
             'imagen': forms.FileInput(attrs={'class': 'form-control'}),
         }
--- articulos/models.py
+++ articulos/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Publicacion(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
+    costo = models.IntegerField()
     vendedor = models.ForeignKey(User, on_delete=models.CASCADE)
     fecha_publicacion = models.DateTimeField(auto_now_add=True)
     imagen = models.ImageField(upload_to='imagenes/')
